4d2539e5c7374fe3eed1b59e263a9ea9b324cab7
Lee Raulin <leeraulin@gmail.com>
2018-11-26 17:17:32 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Tweetinvi;
+
+namespace TwitterTest
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Auth.SetUserCredentials(CONSUMER_KEY, CONSUMER_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+            var matchingTweets = Search.SearchTweets("transportation");
+            var json = Tweetinvi.JsonSerializer.ToJson(matchingTweets);
+            string jsonFormatted = JValue.Parse(json).ToString(Formatting.Indented);
+            Console.WriteLine(jsonFormatted);
+        }
+    }
+}
